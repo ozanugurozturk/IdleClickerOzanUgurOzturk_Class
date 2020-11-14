a4d7879acc2394f1c18fecf9bdd76de514b53a3b
Ozan Ugur Ozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2020-11-14 20:57:55 +0100

    Produce Gold Button & Script added

---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Gold : MonoBehaviour
+{
+    public int goldAmount;
+    public Text goldAmountText;
+
+    private void Update()
+    {
+        this.goldAmountText.text = this.goldAmount.ToString("0");
+    }
+
+
+    public void ProduceGold()
+    {
+        this.goldAmount = this.goldAmount + 5;
+    }
+}
+ 

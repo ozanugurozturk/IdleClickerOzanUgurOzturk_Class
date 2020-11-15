9dc1b23c7f69d6f54e604e75e8d1b2ef8150f604
Ozan Ugur Ozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2020-11-15 14:47:30 +0100

    Creating GoldPress Prefab

---
+++ Assets/Scripts/GoldProductionUnits.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldProductionUnits : MonoBehaviour
+{
+    [System.Serializable]
+    public class GoldProductionUnit
+    {
+        public string name = "GoldPress";
+        public int productionAmount = 1;
+        public int costs = 100;
+        public float productionTime = 1f;        
+    }
+
+    public GoldProductionUnit[] goldProductionUnits;
+
+    void Start()
+    {
+        foreach (var productionUnit in this.goldProductionUnits)
+        {
+
+        }
+    }
+
+
+
+}
